c0f461e599048875f083e99d1626730b04c896c1
Arthur Miller <arthur.miller@live.com>
2021-08-15 02:05:25 +0200

    Local branches

--- helm-git-branch.el
+++ helm-git-branch.el
@@ -31,16 +31,6 @@
 It can be build explicitly with function
 `helm-git-build-local-branches-source'.")
 
-(defvar helm-source-git-remote-branches nil
-  "This source will built at runtime.
-It can be build explicitly with function
-`helm-git-build-remote-branches-source'.")
-
-(defvar helm-source-git-remote-all-branches nil
-  "This source will built at runtime.
-It can be build explicitly with function
-`helm-build-remote-all-branches-source'.")
-
 (defface helm-git-branch-current-clean-face
     '((t :foreground "#2aa198"))
   "Files which contain rebase/merge conflicts."
@@ -80,9 +70,7 @@ If this option is set to 'commit the changes will automatically commited."
   :group 'helm-branch-git
   :set (lambda (var val)
          (set var val)
-         (setq helm-source-git-local-branches nil
-               helm-source-git-remote-branches nil
-               helm-source-git-remote-all-branches nil))
+         (setq helm-source-git-local-branches nil))
   :type 'boolean)
 
 
@@ -128,30 +116,6 @@ If this option is set to 'commit the changes will automatically commited."
 (defun helm-git--local-branches ()
   (with-helm-git "branch"))
 
-(defvar-local helm-git--cache-remote-heads nil)
-(defvar-local helm-git--cache-remote-tracked nil)
-
-(defun helm-git--remote-branches ()
-  (unless helm-git--cache-remote-tracked
-    (setq-local helm-git--cache-remote-tracked
-                (with-helm-git
-                  "branch -r"
-                  (replace-all "origin/" ""))))
-  (cdr helm-git--cache-remote-tracked))
-
-(defun helm-git--remote-all-branches ()
-  (unless helm-git--cache-remote-heads
-    (setq-local helm-git--cache-remote-heads
-                (with-helm-git
-                  "ls-remote --heads"
-                  (replace-all "refs/heads/" ""))))
-  (let (accum temp)
-    (dolist (head (cdr helm-git--cache-remote-heads))
-      (setq temp (split-string head))
-      (setq temp (or (cdr temp) (car temp)))
-      (push (car temp) accum))
-    (nreverse accum)))
-
 (defun helm-git-branch-local-transformer (candidates _source)
   (cl-loop with root = (helm-ls-git-root-dir)
         for i in candidates
@@ -165,86 +129,17 @@ If this option is set to 'commit the changes will automatically commited."
               (t i))))
 
 (defun helm-git-branch-local-action-transformer (actions _candidate)
-  (let ((disp (helm-get-selection nil t))
-        (action 
-         (when (= 1 (length (helm-marked-candidates)))
-           (helm-make-actions "Checkout branch"
-                              (lambda (_candidate)
-                                (let ((marked (helm-marked-candidates)))
-                                  (cl-loop for f in marked do
-                                           (progn
-                                             (vc-git-revert f)
-                                             (helm-aif (get-file-buffer f)
-                                                 (with-current-buffer it
-                                                   (revert-buffer t t)))))))))))
-    ;; Unregistered files
-    (cond ((string-match "^[?]\\{2\\}" disp)
-           (append actions
-                   (list '("Add file(s)"
-                           . (lambda (candidate)
-                               (let ((default-directory
-                                      (file-name-directory candidate))
-                                     (marked (helm-marked-candidates)))
-                                 (vc-call-backend 'Git 'register marked))))
-                         '("Delete file(s)" . helm-ff-delete-files)
-                         '("Copy bnames to .gitignore"
-                           . (lambda (candidate)
-                               (let ((default-directory
-                                      (file-name-directory candidate))
-                                     (marked (helm-marked-candidates)))
-                                 (with-current-buffer (find-file-noselect
-                                                       (expand-file-name
-                                                        ".gitignore"
-                                                        (helm-ls-git-root-dir)))
-                                   (goto-char (point-max))
-                                   (cl-loop with last-bname 
-                                         for f in marked
-                                         for bname = (helm-basename f)
-                                         unless (string= bname last-bname)
-                                         do (insert (concat bname "\n"))
-                                         do (setq last-bname bname))
-                                   (save-buffer))))))))
-          ((string-match "^A " disp)
-           (append actions '(("Commit staged file(s)"
-                              . helm-ls-git-commit)
-                             ("Extend commit"
-                              . helm-ls-git-extend-commit)
-                             ("Amend commit"
-                              . helm-ls-git-amend-commit)
-                             ("Unstage file(s)"
-                              . helm-ls-git-unstage-files))))
-          ;; Modified but not staged
-          ((string-match "^ M+ *" disp)
-           (append actions (helm-append-at-nth
-                            action
-                            '(("Stage file(s)"
-                               . helm-ls-git-stage-files)
-                              ("Stage marked file(s) and commit"
-                               . helm-ls-git-stage-marked-and-commit)
-                              ("Stage marked file(s) and extend commit"
-                               . helm-ls-git-stage-marked-and-extend-commit)
-                              ("Stage marked file(s) and amend commit"
-                               . helm-ls-git-stage-marked-and-amend-commit))
-                            1)))
-          ;; Modified and staged
-          ((string-match "^M+ *" disp)
-           (append actions (helm-append-at-nth
-                            action
-                            '(("Commit staged file(s)"
-                               . helm-ls-git-commit)
-                              ("Extend commit"
-                               . helm-ls-git-extend-commit)
-                              ("Amend commit"
-                               . helm-ls-git-amend-commit)
-                              ("Unstage file(s)"
-                               . helm-ls-git-unstage-files))
-                            1)))
-          ;; Deleted
-          ((string-match "^ D " disp)
-           (append actions (list '("Git delete" . vc-git-delete-file)
-                                 '("Stage file(s)"
-                                   . helm-ls-git-stage-files))))
-          (t actions))))
+  (when (= 1 (length (helm-marked-candidates)))
+    (helm-make-actions "Checkout branch"
+                       (lambda (_candidate)
+                         (let ((marked (helm-marked-candidates)))
+                           (cl-loop for f in marked do
+                                    (progn
+                                      (vc-git-revert f)
+                                      (helm-aif (get-file-buffer f)
+                                          (with-current-buffer it
+                                            (revert-buffer t t))))))))
+    actions))
 
 (defclass helm-git-local-source (helm-source-in-buffer)
   ((header-name :initform 'helm-ls-git-header-name)
@@ -259,46 +154,12 @@ If this option is set to 'commit the changes will automatically commited."
    (filtered-candidate-transformer :initform 'helm-git-branch-local-transformer)
    (action-transformer :initform 'helm-git-branch-local-action-transformer)))
 
-(defclass helm-git-remote-source (helm-source-in-buffer)
-  ((header-name :initform 'helm-ls-git-header-name)
-   (init :initform
-         (lambda ()
-           (helm-init-candidates-in-buffer 'global
-             (helm-git--remote-branches))))
-   (keymap :initform 'helm-git-branch-map)
-   ;;(filtered-candidate-transformer :initform 'helm-ls-git-branches-transformer)
-   ;;(action-transformer :initform 'helm-ls-git-branches-action-transformer)
-   ))
-
-(defclass helm-git-remote-all-source (helm-source-in-buffer)
-  ((header-name :initform 'helm-ls-git-header-name)
-   (init :initform
-         (lambda ()
-           (helm-init-candidates-in-buffer 'global
-             (helm-git--remote-all-branches))))
-   (keymap :initform 'helm-git-branch-map)
-   ;;(filtered-candidate-transformer :initform 'helm-ls-git-branches-transformer)
-   ;;(action-transformer :initform 'helm-ls-git-branches-action-transformer)
-   ))
-
 (defun helm-git-build-local-branches-source ()
   ;;(and (memq 'helm-source-ls-git-branches helm-ls-git-default-sources)
   (helm-make-source "Local branches" 'helm-git-local-source
     :fuzzy-match helm-git-branch-fuzzy-match
     :group 'helm
     :keymap helm-git-branch-map))
-
-(defun helm-git-build-remote-branches-source ()
-  ;;(and (memq 'helm-source-ls-git-branches helm-ls-git-default-sources)
-  (helm-make-source "Tracked remote branches" 'helm-git-remote-source
-    :fuzzy-match helm-git-branch-fuzzy-match
-    :group 'helm))
-
-(defun helm-git-build-remote-all-branches-source ()
-  ;;(and (memq 'helm-source-ls-git-branches helm-ls-git-default-sources)
-  (helm-make-source "All remote branches" 'helm-git-remote-all-source
-    :fuzzy-match helm-git-branch-fuzzy-match
-    :group 'helm))
 
 
 
@@ -355,40 +216,6 @@ If this option is set to 'commit the changes will automatically commited."
     (helm-set-local-variable
      'helm-ls-git--current-branch (helm-ls-git--branch))
     (helm :sources helm-source-git-local-branches
-          :ff-transformer-show-only-basename nil
-          :truncate-lines helm-buffers-truncate-lines
-          :buffer "*helm branches*")))
-
-;;;###autoload
-(defun helm-git-remote-branches (&optional arg)
-  (interactive "p")
-  (let ((helm-ff-default-directory
-         (or helm-ff-default-directory
-             default-directory)))
-    (when (and arg (not (helm-ls-git-root-dir)))
-      (error "Not inside a Git repository"))
-    (setq helm-source-git-remote-branches
-          (helm-git-build-remote-branches-source))
-    (helm-set-local-variable
-     'helm-ls-git--current-branch (helm-ls-git--branch))
-    (helm :sources helm-source-git-remote-branches
-          :ff-transformer-show-only-basename nil
-          :truncate-lines helm-buffers-truncate-lines
-          :buffer "*helm branches*")))
-
-;;;###autoload
-(defun helm-git-remote-all-branches (&optional arg)
-  (interactive "p")
-  (let ((helm-ff-default-directory
-         (or helm-ff-default-directory
-             default-directory)))
-    (when (and arg (not (helm-ls-git-root-dir)))
-      (error "Not inside a Git repository"))
-    (helm-set-local-variable
-     'helm-ls-git--current-branch (helm-ls-git--branch))
-    (setq helm-source-git-remote-all-branches
-          (helm-git-build-remote-all-branches-source))
-    (helm :sources helm-source-git-remote-all-branches
           :ff-transformer-show-only-basename nil
           :truncate-lines helm-buffers-truncate-lines
           :buffer "*helm branches*")))
